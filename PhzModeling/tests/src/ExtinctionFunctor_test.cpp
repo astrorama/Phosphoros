f7d038dd4ccbddee9d9e1d0402b284d4ab86bfe6
Pierre Dubath <Pierre.Dubath@unige.ch>
2014-09-29 09:06:26 +0000

    Extinction functor tests
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Phosphoros/trunk@5817 463500fe-d6be-4184-9b20-47717e75ecac

--- PhzModeling/tests/src/ExtinctionFunctor_test.cpp
+++ PhzModeling/tests/src/ExtinctionFunctor_test.cpp
@@ -66,14 +66,74 @@ BOOST_AUTO_TEST_SUITE (ExtinctionFunctor_test)
 //-----------------------------------------------------------------------------
 BOOST_FIXTURE_TEST_CASE(outputLength_test, ExtinctionFunctor_Fixture) {
   BOOST_TEST_MESSAGE(" ");
-  BOOST_TEST_MESSAGE("--> Testing the length of the returned SED");
+  BOOST_TEST_MESSAGE("--> Testing the fixed point");
   BOOST_TEST_MESSAGE(" ");
 
   auto output_sed=functor(input_sed,extinction_function,0.);
   BOOST_CHECK_EQUAL(input_sed.size(), output_sed.size());
 }
 
-// TODO : more to come
+//-----------------------------------------------------------------------------
+// Check that the functor returns a XYDataSet with the same values if EVB=0
+//-----------------------------------------------------------------------------
+BOOST_FIXTURE_TEST_CASE(fixPoint_test, ExtinctionFunctor_Fixture) {
+  BOOST_TEST_MESSAGE(" ");
+  BOOST_TEST_MESSAGE("--> Testing the fix point");
+  BOOST_TEST_MESSAGE(" ");
+
+  auto output_sed=functor(input_sed,extinction_function,0.);
+  auto output_iterator = output_sed.begin();
+  for (auto& input_pair : input_sed) {
+    BOOST_CHECK(Elements::isEqual(input_pair.first,output_iterator->first));
+    BOOST_CHECK(Elements::isEqual(input_pair.second,output_iterator->second));
+    output_iterator++;
+  }
+}
+
+//-----------------------------------------------------------------------------
+// Check that the functor has the right dependence in EVB
+//-----------------------------------------------------------------------------
+BOOST_FIXTURE_TEST_CASE(evbDependency_test, ExtinctionFunctor_Fixture) {
+  BOOST_TEST_MESSAGE(" ");
+  BOOST_TEST_MESSAGE("--> Testing the dependence in EVB");
+  BOOST_TEST_MESSAGE(" ");
+  auto evb_ref=0.7;
+
+  auto output_sed=functor(input_sed,extinction_function,evb_ref);
+  for(auto evb=1;evb!=4;++evb){
+
+    auto output_sed_2=functor(input_sed,extinction_function,evb);
+    auto output_iterator = output_sed.begin();
+    auto output_iterator_2 = output_sed_2.begin();
+    for (auto& input_pair : input_sed) {
+      auto factor_for_output_1 = output_iterator->second/input_pair.second;
+      auto factor_for_output_2 = output_iterator_2->second/input_pair.second;
+      BOOST_CHECK(Elements::isEqual(std::pow(factor_for_output_1,evb/evb_ref),factor_for_output_2));
+
+      ++output_iterator;
+      ++output_iterator_2;
+    }
+  }
+}
+
+//-----------------------------------------------------------------------------
+// Check that the functor has the right dependence in the extinction function
+//-----------------------------------------------------------------------------
+BOOST_FIXTURE_TEST_CASE(extinctionFunctionDependency_test, ExtinctionFunctor_Fixture) {
+  BOOST_TEST_MESSAGE(" ");
+  BOOST_TEST_MESSAGE("--> Testing the dependence in the extinction function");
+  BOOST_TEST_MESSAGE(" ");
+
+  auto output_sed=functor(input_sed,extinction_function,0.7);
+  auto output_iterator = output_sed.begin();
+  for (auto& input_pair : input_sed) {
+    auto factor = output_iterator->second/input_pair.second;
+    auto k_lambda=extinction_function(input_pair.first);
+
+    BOOST_CHECK(Elements::isEqual(std::pow(10,-0.4*0.7),std::pow(factor,1/k_lambda)));
+    ++output_iterator;
+  }
+}
 
 
 BOOST_AUTO_TEST_SUITE_END ()
