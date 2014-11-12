150dd4b7e814529704d72f819ac4b6dfe0e9f615
Nicolas Morisset <Nicolas.Morisset@unige.ch>
2014-11-12 15:05:32 +0000

    - Added comments to the CreatePhotometryGridConfiguration class
    [Redmine Issue #6914]
    
    
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Phosphoros/trunk@6227 463500fe-d6be-4184-9b20-47717e75ecac

--- PhzConfiguration/PhzConfiguration/CreatePhotometryGridConfiguration.h
+++ PhzConfiguration/PhzConfiguration/CreatePhotometryGridConfiguration.h
@@ -10,14 +10,31 @@
 #include <functional>
 #include <map>
 #include <string>
-#include <fstream>
+
 #include <boost/program_options.hpp>
+
 #include "PhzDataModel/PhotometryGrid.h"
 #include "PhzConfiguration/ParameterSpaceConfiguration.h"
 #include "PhzConfiguration/FilterConfiguration.h"
 
 namespace Euclid {
 namespace PhzConfiguration {
+
+/**
+ * @class Euclid::PhzConfiguration::CreatePhotometryGridConfiguration
+ * @brief
+ * This class defines the Photometry Grid parameter option, binary-photometry-grid
+ * and inherits from the one of ParameterSpaceConfiguration and FilterConfiguration
+ * options. It also provides a function for writing a grid in a binary file.
+ * @details
+ * The parameter available is :
+ *  binary-photometry-grid : string, output filename for storing the grid data
+ * Before writing data to the disk, the constructor checks that it is
+ * possible to write on the disk at the location specified by the
+ * binary-photometry-grid option and throws an exception if any.
+ * @throw
+ * ElementException:  IO error, can not write any file there
+ */
 
 class CreatePhotometryGridConfiguration : public ParameterSpaceConfiguration,
                                           public FilterConfiguration {
@@ -26,10 +43,42 @@ public:
   
   typedef std::function<void(const PhzDataModel::PhotometryGrid&)> OutputFunction;
   
+  /**
+   * @brief
+   * Get program options of the CreatePhotometryGridConfiguration class
+   * @return
+   * A boost boost::program_options::options_description type
+   */
   static boost::program_options::options_description getProgramOptions();
   
+  /**
+   * @brief Constructor
+   * The CreatePhotometryGridConfiguration class which defines parameters allowed for
+   * the CreatePhotometryGridConfiguration parameter options
+   *
+   * @details
+   * The option is:
+   *  binary-photometry-grid : string, output filename for storing the grid data
+   * @param options
+   * A map containing the options and their value.
+   *
+   */
   CreatePhotometryGridConfiguration(const std::map<std::string, boost::program_options::variable_value>& options);
   
+  /**
+   * @brief
+   * This function provides a function of PhzDataModel::PhotometryGrid
+   * object and stores this object in a binary file with the filename provided
+   * by the binary-photometry-grid option.
+   * @details
+   * Before writing the object to the disk, the constructor checks that it is
+   * possible to write data on the disk at the location specified by the
+   * binary-photometry-grid option and throws an exception if any.
+   * @throw ElementException
+   * ElementException:  IO error, can not write any file there
+   * @return
+   * A void function.
+   */
   OutputFunction getOutputFunction();
   
 private:
