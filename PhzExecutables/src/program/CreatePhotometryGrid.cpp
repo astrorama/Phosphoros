3bcb067731772f360f9617548c105fea053c6e7d
Nikolaos Apostolakos <nikoapos@gmail.com>
2014-11-20 16:21:35 +0000

    Added the executable for generating photometry grids.
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Phosphoros/trunk@6344 463500fe-d6be-4184-9b20-47717e75ecac

---
+++ PhzExecutables/src/program/CreatePhotometryGrid.cpp
@@ -0,0 +1,55 @@
+/** 
+ * @file CreatePhotometryGrid.cpp
+ * @date November 20, 2014
+ * @author Nikolaos Apostolakos
+ */
+
+#include <map>
+#include <string>
+#include <boost/program_options.hpp>
+#include "ElementsKernel/Program.h"
+#include "ElementsKernel/Logging.h"
+#include "PhzConfiguration/CreatePhotometryGridConfiguration.h"
+#include "PhzModeling/PhotometryGridCreator.h"
+
+using namespace std;
+using namespace Euclid;
+namespace po = boost::program_options;
+
+static Elements::Logging logger = Elements::Logging::getLogger("CreatePhotometryGrid");
+
+class CreatePhotometryGrid : public Elements::Program {
+
+public:
+  
+  po::options_description defineSpecificProgramOptions() override {
+    return PhzConfiguration::CreatePhotometryGridConfiguration::getProgramOptions();
+  }
+  
+  Elements::ExitCode mainMethod(map<string, po::variable_value>& args) override {
+
+    PhzConfiguration::CreatePhotometryGridConfiguration conf {args};
+    
+    PhzModeling::PhotometryGridCreator creator {conf.getSedDatasetProvider(),
+                                                conf.getReddeningDatasetProvider(),
+                                                conf.getFilterDatasetProvider()};
+    
+    auto param_space = PhzDataModel::createAxesTuple(conf.getZList(), conf.getEbvList(),
+                                                     conf.getReddeningCurveList(),
+                                                     conf.getSedList());
+    
+    auto grid = creator.createGrid(param_space, conf.getFilterList());
+    
+    auto output = conf.getOutputFunction();
+    output(grid);
+    
+    return Elements::ExitCode::OK;
+  }
+  
+  string getVersion() override {
+    return "0.1";
+  }
+  
+};
+
+MAIN_FOR(CreatePhotometryGrid)
