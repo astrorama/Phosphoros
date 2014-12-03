912738e748e2447ac98b2d412a72321490d3c5c7
Nikolaos Apostolakos <nikoapos@gmail.com>
2014-12-03 16:59:58 +0000

    Implemented the CreatePhzCatalogConfiguration class [Redmine Issue #6979]
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Phosphoros/trunk@6453 463500fe-d6be-4184-9b20-47717e75ecac

---
+++ PhzConfiguration/PhzConfiguration/CreatePhzCatalogConfiguration.h
@@ -0,0 +1,39 @@
+/** 
+ * @file CreatePhzCatalogConfiguration.h
+ * @date December 3, 2014
+ * @author Nikolaos Apostolakos
+ */
+
+#ifndef PHZCONFIGURATION_CREATEPHZCATALOGCONFIGURATION_H
+#define	PHZCONFIGURATION_CREATEPHZCATALOGCONFIGURATION_H
+
+#include <map>
+#include <string>
+#include <boost/program_options.hpp>
+#include "PhzOutput/OutputHandler.h"
+#include "PhzConfiguration/PhotometricCorrectionConfiguration.h"
+
+namespace Euclid {
+namespace PhzConfiguration {
+
+class CreatePhzCatalogConfiguration : public PhotometricCorrectionConfiguration {
+  
+public:
+  
+  static boost::program_options::options_description getProgramOptions();
+  
+  CreatePhzCatalogConfiguration(const std::map<std::string, boost::program_options::variable_value>& options);
+  
+  std::unique_ptr<PhzOutput::OutputHandler> getOutputHandler();
+  
+private:
+  
+  std::map<std::string, boost::program_options::variable_value> m_options;
+  
+};
+
+} // end of namespace PhzConfiguration
+} // end of namespace Euclid
+
+#endif	/* PHZCONFIGURATION_CREATEPHZCATALOGCONFIGURATION_H */
+
---
+++ PhzConfiguration/src/lib/CreatePhzCatalogConfiguration.cpp
@@ -0,0 +1,38 @@
+/** 
+ * @file CreatePhzCatalogConfiguration.cpp
+ * @date December 3, 2014
+ * @author Nikolaos Apostolakos
+ */
+
+#include "PhzOutput/BestModelCatalog.h"
+#include "PhzConfiguration/CreatePhzCatalogConfiguration.h"
+
+namespace po = boost::program_options;
+
+namespace Euclid {
+namespace PhzConfiguration {
+
+po::options_description CreatePhzCatalogConfiguration::getProgramOptions() {
+  boost::program_options::options_description options {"Create PHZ Catalog options"};
+
+  options.add_options()
+  ("output-catalog-file", po::value<std::string>(),
+      "The filename of the file to export the PHZ catalog file");
+
+  options.add(PhotometricCorrectionConfiguration::getProgramOptions());
+
+  return options;
+}
+
+CreatePhzCatalogConfiguration::CreatePhzCatalogConfiguration(const std::map<std::string, po::variable_value>& options)
+          : CatalogConfiguration(options), PhotometricCorrectionConfiguration(options) {
+  m_options = options;
+}
+
+std::unique_ptr<PhzOutput::OutputHandler> CreatePhzCatalogConfiguration::getOutputHandler() {
+  std::string out_file = m_options["output-catalog-file"].as<std::string>();
+  return std::unique_ptr<PhzOutput::OutputHandler> {new PhzOutput::BestModelCatalog{out_file}};
+}
+
+} // end of namespace PhzConfiguration
+} // end of namespace Euclid
