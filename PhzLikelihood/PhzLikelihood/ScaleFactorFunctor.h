88d2548ec7e7913e25d0458d423b765a88429e06
Florian Dubath <Florian.Dubath@unige.ch>
2014-12-15 11:27:28 +0000

    Comment and tests for the Scale Factor Functor
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Phosphoros/trunk@6550 463500fe-d6be-4184-9b20-47717e75ecac

--- PhzLikelihood/PhzLikelihood/ScaleFactorFunctor.h
+++ PhzLikelihood/PhzLikelihood/ScaleFactorFunctor.h
@@ -1,4 +1,4 @@
-/** 
+/**
  * @file ScaleFactorFunctor.h
  * @date December 2, 2014
  * @author Nikolaos Apostolakos
@@ -9,15 +9,42 @@
 
 namespace Euclid {
 namespace PhzLikelihood {
-
+/**
+ * @class Euclid::PhzLikelihood::ScaleFactorFunctor
+ * @brief
+ * This functor is in charge of computing the Scale Factor of the source
+ * @details
+ * For a single Filter the Scale Factor is the ratio between the source flux and the model flux.
+ * When multiple filter are available the Scale Factor is computed as the ratio between the
+ * weighted sum of the source flux and the weighted sum of the model flux.
+ * The applied weight being the model flux over the (source) error squared.
+ * The model is assumed to be error free.
+ */
 class ScaleFactorFunctor {
-  
+
 public:
-  
+
+  /**
+    * @brief Function Call Operator
+    * @details
+    * Compute the Scale Factor of a source
+    *
+    * @param source_begin
+    * An iterator over the Filters of the source exposing an object having "flux" and "error" fields.
+
+    * @param source_end
+    * End iterator for the Filters of the source.
+    *
+    * @param model_begin
+    * An iterator over the Filters of the model exposing an object containing the model "flux".
+    *
+    * @return
+    * The computed Scale Factor as a double.
+    */
   template<typename SourceIter, typename ModelIter>
   double operator()(SourceIter source_begin, SourceIter source_end,
                     ModelIter model_begin) const;
-  
+
 };
 
 } // end of namespace PhzLikelihood
