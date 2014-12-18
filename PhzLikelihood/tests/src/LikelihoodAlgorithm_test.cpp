909ad53356737e369ec25028557e133a910a18a4
Nikolaos Apostolakos <nikoapos@gmail.com>
2014-12-18 13:57:50 +0000

    Modified the LikelihoodAlgorithm_test to use the google mock framework.
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Phosphoros/trunk@6608 463500fe-d6be-4184-9b20-47717e75ecac

--- PhzLikelihood/tests/src/LikelihoodAlgorithm_test.cpp
+++ PhzLikelihood/tests/src/LikelihoodAlgorithm_test.cpp
@@ -11,6 +11,42 @@
 #include "LikelihoodCalcMock.h"
 
 using namespace Euclid;
+using namespace std::placeholders;
+
+class LikelihoodAlgorithmFixture {
+  
+public:
+  
+  std::shared_ptr<std::vector<std::string>> source_filters {new std::vector<std::string> {
+    "Filter1", "Filter2", "Filter3"
+  }};
+  std::vector<SourceCatalog::FluxErrorPair> source_fluxes {
+    {1.,1.}, {2.,2.}, {3.,3.}
+  };
+  SourceCatalog::Photometry source_phot {source_filters, source_fluxes};
+  
+  int model_no = 5;
+  std::shared_ptr<std::vector<std::string>> model_filters {new std::vector<std::string> {
+    "Filter1", "Filter2", "Filter3"
+  }};
+  std::vector<SourceCatalog::Photometry> model_phot_list = makeModelPhotList(model_no, model_filters);
+  
+private:
+  
+  std::vector<SourceCatalog::Photometry> makeModelPhotList(int model_no,
+                                std::shared_ptr<std::vector<std::string>> model_filters) {
+    std::vector<SourceCatalog::Photometry> result {};
+    for (int i=0; i<model_no; ++i) {
+      std::vector<SourceCatalog::FluxErrorPair> model_fluxes;
+      model_fluxes.emplace_back(i*3,i*3);
+      model_fluxes.emplace_back(i*3+1,i*3+1);
+      model_fluxes.emplace_back(i*3+2,i*3+2);
+      result.emplace_back(model_filters, model_fluxes);
+    }
+    return result;
+  }
+  
+};
 
 //-----------------------------------------------------------------------------
 
@@ -20,58 +56,29 @@ BOOST_AUTO_TEST_SUITE (LikelihoodAlgorithm_test)
 // Check that the algorithm works with the same filter order between source and
 // models.
 //-----------------------------------------------------------------------------
-BOOST_AUTO_TEST_CASE(SameFilterOrder) {
+BOOST_FIXTURE_TEST_CASE(SameFilterOrder, LikelihoodAlgorithmFixture) {
   
   // Given
-  std::shared_ptr<std::vector<std::string>> source_filters {new std::vector<std::string>};
-  source_filters->push_back("Filter1");
-  source_filters->push_back("Filter2");
-  source_filters->push_back("Filter3");
-  std::vector<SourceCatalog::FluxErrorPair> source_fluxes;
-  source_fluxes.push_back({1.,1.});
-  source_fluxes.push_back({2.,2.});
-  source_fluxes.push_back({3.,3.});
-  SourceCatalog::Photometry source_phot {source_filters, source_fluxes};
-  
-  std::shared_ptr<std::vector<std::string>> model_filters {new std::vector<std::string>};
-  model_filters->push_back("Filter1");
-  model_filters->push_back("Filter2");
-  model_filters->push_back("Filter3");
-  std::vector<SourceCatalog::Photometry> model_phot_list {};
-  int model_no = 5;
-  for (int i=0; i<model_no; ++i) {
-    std::vector<SourceCatalog::FluxErrorPair> model_fluxes;
-    model_fluxes.emplace_back(i*3,i*3);
-    model_fluxes.emplace_back(i*3+1,i*3+1);
-    model_fluxes.emplace_back(i*3+2,i*3+2);
-    model_phot_list.emplace_back(model_filters, model_fluxes);
-  }
-  
   ScaleFactorCalcMock scale_factor_calc_mock;
   LikelihoodCalcMock likelihood_calc_mock;
-  
   std::vector<double> likelihood_list (model_no);
   
   // Expect
+  InSequence in_sequence;
   for (int i=0; i<model_no; ++i) {
-    scale_factor_calc_mock.expect_functor_call(source_phot.begin(), source_phot.end(), model_phot_list[i].begin(), i);
-    likelihood_calc_mock.expect_functor_call(source_phot.begin(), source_phot.end(), model_phot_list[i].begin(), i, i);
+    scale_factor_calc_mock.expectFunctorCall(source_phot, model_phot_list[i], i);
+    likelihood_calc_mock.expectFunctorCall(source_phot, model_phot_list[i], i, i);
   }
   
   // When
-  PhzLikelihood::LikelihoodAlgorithm likelihood_algo {std::move(scale_factor_calc_mock), std::move(likelihood_calc_mock)};
+  PhzLikelihood::LikelihoodAlgorithm likelihood_algo {
+            std::bind(&ScaleFactorCalcMock::FunctorCall, &scale_factor_calc_mock, _1, _2, _3),
+            std::bind(&LikelihoodCalcMock::FunctorCall, &likelihood_calc_mock, _1, _2, _3, _4)};
   likelihood_algo(source_phot, model_phot_list.begin(), model_phot_list.end(), likelihood_list.begin());
   
   // Then
   for (int i=0; i<model_no; ++i) {
-// We have instruct the mock class to return the exact double representation. We
-// want to do a very strict bitwise comparison and not a more relaxed real value
-// representation comparison, to guarantee that the algorithm does not do any
-// math on the result. The following pragma supress the related warnings.
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wfloat-equal"
     BOOST_CHECK(likelihood_list[i] == i);
-#pragma GCC diagnostic pop
   }
   
 }
@@ -80,44 +87,20 @@ BOOST_AUTO_TEST_CASE(SameFilterOrder) {
 // Check that the algorithm works with different filter order between source and
 // models.
 //-----------------------------------------------------------------------------
-BOOST_AUTO_TEST_CASE(DifferentFilterOrder) {
+BOOST_FIXTURE_TEST_CASE(DifferentFilterOrder, LikelihoodAlgorithmFixture) {
   
   // Given
-  std::shared_ptr<std::vector<std::string>> source_filters {new std::vector<std::string>};
-  source_filters->push_back("Filter2");
-  source_filters->push_back("Filter1");
-  source_filters->push_back("Filter4");
-  source_filters->push_back("Filter3");
-  std::vector<SourceCatalog::FluxErrorPair> source_fluxes;
-  source_fluxes.push_back({2.,2.});
-  source_fluxes.push_back({1.,1.});
-  source_fluxes.push_back({1.,4.});
-  source_fluxes.push_back({3.,3.});
-  SourceCatalog::Photometry source_phot {source_filters, source_fluxes};
-  
-  std::shared_ptr<std::vector<std::string>> source_filters_ord {new std::vector<std::string>};
-  source_filters_ord->push_back("Filter1");
-  source_filters_ord->push_back("Filter2");
-  source_filters_ord->push_back("Filter3");
-  std::vector<SourceCatalog::FluxErrorPair> source_fluxes_ord;
-  source_fluxes_ord.push_back({1.,1.});
-  source_fluxes_ord.push_back({2.,2.});
-  source_fluxes_ord.push_back({3.,3.});
-  SourceCatalog::Photometry source_phot_ord {source_filters_ord, source_fluxes_ord};
-  
-  std::shared_ptr<std::vector<std::string>> model_filters {new std::vector<std::string>};
-  model_filters->push_back("Filter1");
-  model_filters->push_back("Filter2");
-  model_filters->push_back("Filter3");
-  std::vector<SourceCatalog::Photometry> model_phot_list {};
-  int model_no = 5;
-  for (int i=0; i<model_no; ++i) {
-    std::vector<SourceCatalog::FluxErrorPair> model_fluxes;
-    model_fluxes.emplace_back(i*3,i*3);
-    model_fluxes.emplace_back(i*3+1,i*3+1);
-    model_fluxes.emplace_back(i*3+2,i*3+2);
-    model_phot_list.emplace_back(model_filters, model_fluxes);
-  }
+  std::shared_ptr<std::vector<std::string>> source_filters_unordered {new std::vector<std::string>};
+  source_filters_unordered->push_back("Filter2");
+  source_filters_unordered->push_back("Filter1");
+  source_filters_unordered->push_back("Filter4");
+  source_filters_unordered->push_back("Filter3");
+  std::vector<SourceCatalog::FluxErrorPair> source_fluxes_unordered;
+  source_fluxes_unordered.push_back({2.,2.});
+  source_fluxes_unordered.push_back({1.,1.});
+  source_fluxes_unordered.push_back({1.,4.});
+  source_fluxes_unordered.push_back({3.,3.});
+  SourceCatalog::Photometry source_phot_unordered {source_filters_unordered, source_fluxes_unordered};
   
   ScaleFactorCalcMock scale_factor_calc_mock;
   LikelihoodCalcMock likelihood_calc_mock;
@@ -125,25 +108,21 @@ BOOST_AUTO_TEST_CASE(DifferentFilterOrder) {
   std::vector<double> likelihood_list (model_no);
   
   // Expect
+  InSequence in_sequence;
   for (int i=0; i<model_no; ++i) {
-    scale_factor_calc_mock.expect_functor_call(source_phot_ord.begin(), source_phot_ord.end(), model_phot_list[i].begin(), i);
-    likelihood_calc_mock.expect_functor_call(source_phot_ord.begin(), source_phot_ord.end(), model_phot_list[i].begin(), i, i);
+    scale_factor_calc_mock.expectFunctorCall(source_phot, model_phot_list[i], i);
+    likelihood_calc_mock.expectFunctorCall(source_phot, model_phot_list[i], i, i);
   }
   
   // When
-  PhzLikelihood::LikelihoodAlgorithm likelihood_algo {std::move(scale_factor_calc_mock), std::move(likelihood_calc_mock)};
-  likelihood_algo(source_phot, model_phot_list.begin(), model_phot_list.end(), likelihood_list.begin());
+  PhzLikelihood::LikelihoodAlgorithm likelihood_algo {
+            std::bind(&ScaleFactorCalcMock::FunctorCall, &scale_factor_calc_mock, _1, _2, _3),
+            std::bind(&LikelihoodCalcMock::FunctorCall, &likelihood_calc_mock, _1, _2, _3, _4)};
+  likelihood_algo(source_phot_unordered, model_phot_list.begin(), model_phot_list.end(), likelihood_list.begin());
   
   // Then
   for (int i=0; i<model_no; ++i) {
-// We have instruct the mock class to return the exact double representation. We
-// want to do a very strict bitwise comparison and not a more relaxed real value
-// representation comparison, to guarantee that the algorithm does not do any
-// math on the result. The following pragma supress the related warnings.
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wfloat-equal"
     BOOST_CHECK(likelihood_list[i] == i);
-#pragma GCC diagnostic pop
   }
   
 }
@@ -151,30 +130,16 @@ BOOST_AUTO_TEST_CASE(DifferentFilterOrder) {
 //-----------------------------------------------------------------------------
 // Check that we get an exception if we miss a photometry
 //-----------------------------------------------------------------------------
-BOOST_AUTO_TEST_CASE(MissingSourcePhotometry) {
+BOOST_FIXTURE_TEST_CASE(MissingSourcePhotometry, LikelihoodAlgorithmFixture) {
   
   // Given
-  std::shared_ptr<std::vector<std::string>> source_filters {new std::vector<std::string>};
-  source_filters->push_back("Filter1");
-  source_filters->push_back("Filter3");
+  std::shared_ptr<std::vector<std::string>> source_filtersmissing {new std::vector<std::string>};
+  source_filtersmissing->push_back("Filter1");
+  source_filtersmissing->push_back("Filter3");
   std::vector<SourceCatalog::FluxErrorPair> source_fluxes;
   source_fluxes.push_back({1.,1.});
   source_fluxes.push_back({3.,3.});
-  SourceCatalog::Photometry source_phot {source_filters, source_fluxes};
-  
-  std::shared_ptr<std::vector<std::string>> model_filters {new std::vector<std::string>};
-  model_filters->push_back("Filter1");
-  model_filters->push_back("Filter2");
-  model_filters->push_back("Filter3");
-  std::vector<SourceCatalog::Photometry> model_phot_list {};
-  int model_no = 5;
-  for (int i=0; i<model_no; ++i) {
-    std::vector<SourceCatalog::FluxErrorPair> model_fluxes;
-    model_fluxes.emplace_back(i*3,i*3);
-    model_fluxes.emplace_back(i*3+1,i*3+1);
-    model_fluxes.emplace_back(i*3+2,i*3+2);
-    model_phot_list.emplace_back(model_filters, model_fluxes);
-  }
+  SourceCatalog::Photometry source_phot_missing {source_filtersmissing, source_fluxes};
   
   ScaleFactorCalcMock scale_factor_calc_mock;
   LikelihoodCalcMock likelihood_calc_mock;
@@ -182,10 +147,14 @@ BOOST_AUTO_TEST_CASE(MissingSourcePhotometry) {
   std::vector<double> likelihood_list (model_no);
   
   // When
-  PhzLikelihood::LikelihoodAlgorithm likelihood_algo {std::move(scale_factor_calc_mock), std::move(likelihood_calc_mock)};
+  PhzLikelihood::LikelihoodAlgorithm likelihood_algo {
+            std::bind(&ScaleFactorCalcMock::FunctorCall, &scale_factor_calc_mock, _1, _2, _3),
+            std::bind(&LikelihoodCalcMock::FunctorCall, &likelihood_calc_mock, _1, _2, _3, _4)};
   
   // Then
-  BOOST_CHECK_THROW(likelihood_algo(source_phot, model_phot_list.begin(), model_phot_list.end(), likelihood_list.begin()), Elements::Exception);
+  BOOST_CHECK_THROW(likelihood_algo(source_phot_missing, model_phot_list.begin(),
+                                    model_phot_list.end(), likelihood_list.begin()),
+                    Elements::Exception);
   
 }
     
--- PhzLikelihood/tests/src/LikelihoodCalcMock.h
+++ PhzLikelihood/tests/src/LikelihoodCalcMock.h
@@ -7,158 +7,57 @@
 #ifndef LIKELIHOODCALCMOCK_H
 #define	LIKELIHOODCALCMOCK_H
 
-#include <queue>
-#include <sstream>
 #include <boost/test/unit_test.hpp>
+#include "EnableGMock.h"
 #include "SourceCatalog/SourceAttributes/Photometry.h"
+
+using namespace testing;
 
 namespace Euclid {
 
+class LikelihoodCalcMock {
+  
+private:
+  
+public:
+  
+  virtual ~LikelihoodCalcMock() = default;
+  
+  typedef SourceCatalog::Photometry::const_iterator phot_iter;
+  
+  MOCK_METHOD4(FunctorCall, double(phot_iter source_begin, phot_iter source_end, phot_iter model_begin, double scale));
+  
+  void expectFunctorCall(const SourceCatalog::Photometry& source, const SourceCatalog::Photometry& model, double scale, double result) {
+    EXPECT_CALL(*this, FunctorCall(_, _, _, scale)).With(Args<0,1,2>(Truly([source, model](std::tuple<phot_iter,phot_iter,phot_iter> args){
+      phot_iter exp_source = source.begin();
+      phot_iter exp_source_end = source.end();
+      phot_iter exp_model = model.begin();
+      phot_iter source = std::get<0>(args);
+      phot_iter source_end = std::get<1>(args);
+      phot_iter model = std::get<2>(args);
 // This is a mock object that performs the more strict bitwise equality checks.
 // It shouldn't contain any logic for real number equality arithmetics. The
 // pragma allows the mock to do exactly that without the unnecessary compilation
 // warnings.
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wfloat-equal"
-
-class LikelihoodCalcMock {
-  
-public:
-  
-  typedef SourceCatalog::Photometry::const_iterator phot_iter;
-  
-  LikelihoodCalcMock() : m_functor_call_queue {new std::queue<std::tuple<phot_iter, phot_iter, phot_iter, double, double>>{}} {}
-  
-  virtual ~LikelihoodCalcMock() {
-    if (m_functor_call_queue.unique() && !m_functor_call_queue->empty()) {
-      std::stringstream message;
-      message << "LikelihoodCalcMock - ";
-      message << "There were " << m_functor_call_queue->size() << " calls to the ";
-      message << "LikelihoodCalc functor that weren't performed";
-      BOOST_FAIL(message.str());
-    }
+      for(; exp_source!=exp_source_end && source!=source_end; ++exp_source, ++ source, ++exp_model, ++model) {
+        if ((*exp_source).flux != (*source).flux || (*exp_source).error != (*source).error) {
+          BOOST_ERROR("Expected source photometry (" << (*exp_source).flux << "," <<
+                      (*exp_source).error << ") but got (" << (*source).flux <<
+                      "," << (*source).error << ") (for filter " << source.filterName() << ")");
+        }
+        if ((*exp_model).flux != (*model).flux || (*exp_model).error != (*model).error) {
+          BOOST_ERROR("Expected model photometry (" << (*exp_model).flux << "," <<
+                      (*exp_model).error << ") but got (" << (*model).flux <<
+                      "," << (*model).error << ") (for filter " << source.filterName() << ")");
+        }
+      }
+      if (exp_source!=exp_source_end || source!=source_end) {
+        BOOST_ERROR("Wrong source photometry size");
+      }
+      return true;
+    }))).WillOnce(Return(result));
   }
-  
-  void reset() {
-    while (!m_functor_call_queue->empty()) {
-      m_functor_call_queue->pop();
-    }
-  }
-  
-  double operator()(phot_iter source_begin, phot_iter source_end, phot_iter model_begin, double scale) {
-    // Check that we expect this function to be called
-    if (m_functor_call_queue->empty()) {
-      BOOST_FAIL("Unexpected LikelihoodCalc functor call");
-    }
-    
-    // Get the parameters that we expect to be called with
-    phot_iter exp_source_begin = std::get<0>(m_functor_call_queue->front());
-    phot_iter exp_source_end = std::get<1>(m_functor_call_queue->front());
-    phot_iter exp_model_begin = std::get<2>(m_functor_call_queue->front());
-    double exp_scale = std::get<3>(m_functor_call_queue->front());
-    double result = std::get<4>(m_functor_call_queue->front());
-    m_functor_call_queue->pop();
-    
-    // Check that the scale is correct
-    if (scale != exp_scale) {
-      std::stringstream message;
-      message << "LikelihoodCalcMock - ";
-      message << "Got scale " << scale << " but expected " << exp_scale;
-      BOOST_ERROR(message.str());
-    }
-    
-    // Check that we have the same number of photometries
-    int phot_counter = 0;
-    for (phot_iter source = source_begin; source != source_end; ++source) {
-      ++phot_counter;
-    }
-    int exp_phot_counter = 0;
-    for (phot_iter source = exp_source_begin; source != exp_source_end; ++source) {
-      ++exp_phot_counter;
-    }
-    if (phot_counter != exp_phot_counter) {
-      std::stringstream message;
-      message << "LikelihoodCalcMock - ";
-      message << "Given source photometry contains fluxes for " << phot_counter;
-      message << " filters but where expected " << exp_phot_counter;
-      BOOST_ERROR(message.str());
-    }
-    
-    // Check that we got the same photometries as we expected
-    for (phot_iter source=source_begin, exp_source=exp_source_begin, model=model_begin, exp_model=exp_model_begin;
-                                              source != source_end; ++source, ++exp_source, ++model, ++exp_model) {
-      
-      // Check that the source filter name is correct
-      if (source.filterName() != exp_source.filterName()) {
-        std::stringstream message;
-        message << "LikelihoodCalcMock - ";
-        message << "Got source photometry for filter " << source.filterName();
-        message << " when expected for filter " << exp_source.filterName();
-        BOOST_ERROR(message.str());
-      }
-      
-      // Check that the source Flux value is correct
-      if ((*source).flux != (*exp_source).flux) {
-        std::stringstream message;
-        message << "LikelihoodCalcMock - ";
-        message << "Got source Flux " << (*source).flux << " when expected " << (*exp_source).flux;
-        message << " (for filter " << source.filterName() << ")";
-        BOOST_ERROR(message.str());
-      }
-      
-      // Check that the source Flux error value is correct
-      if ((*source).error != (*exp_source).error) {
-        std::stringstream message;
-        message << "LikelihoodCalcMock - ";
-        message << "Got source Flux error " << (*source).error << " when expected " << (*exp_source).error;
-        message << " (for filter " << source.filterName() << ")";
-        BOOST_ERROR(message.str());
-      }
-      
-      // Check that the model filter name is correct
-      if (model.filterName() != exp_model.filterName()) {
-        std::stringstream message;
-        message << "LikelihoodCalcMock - ";
-        message << "Got model photometry for filter " << model.filterName();
-        message << " when expected for filter " << exp_model.filterName();
-        BOOST_ERROR(message.str());
-      }
-      
-      // Check that the model Flux value is correct
-      if ((*model).flux != (*exp_model).flux) {
-        std::stringstream message;
-        message << "LikelihoodCalcMock - ";
-        message << "Got model Flux " << (*model).flux << " when expected " << (*exp_model).flux;
-        message << " (for filter " << model.filterName() << ")";
-        BOOST_ERROR(message.str());
-      }
-      
-      // Check that the model Flux error value is correct
-      if ((*model).error != (*exp_model).error) {
-        std::stringstream message;
-        message << "LikelihoodCalcMock - ";
-        message << "Got model Flux error " << (*model).error << " when expected " << (*exp_model).error;
-        message << " (for filter " << model.filterName() << ")";
-        BOOST_ERROR(message.str());
-      }
-    }
-    
-    // Returned the expected value
-    return result;
-  }
-  
-  void expect_functor_call(phot_iter source_begin, phot_iter source_end, phot_iter model_begin, double scale, double result) {
-    m_functor_call_queue->emplace(source_begin, source_end, model_begin, scale, result);
-  }
-  
-private:
-  
-  std::shared_ptr<std::queue<std::tuple<phot_iter, phot_iter, phot_iter, double, double>>> m_functor_call_queue;
-  
-}; // end of class LikelihoodCalcMock
-
-// Recover the warning messages for floating point comparison
-#pragma GCC diagnostic pop
+};
 
 } // end of namespace Euclid
 
