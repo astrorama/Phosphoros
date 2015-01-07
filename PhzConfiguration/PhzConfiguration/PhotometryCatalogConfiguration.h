ffc7813d7dc64a8c42c526d3697486fe26f978d4
Nikolaos Apostolakos <nikoapos@gmail.com>
2015-01-07 11:12:00 +0000

    Documented the class PhotometryCatalogConfiguration [Redmine Issue #7117]
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Phosphoros/trunk@6660 463500fe-d6be-4184-9b20-47717e75ecac

--- PhzConfiguration/PhzConfiguration/PhotometryCatalogConfiguration.h
+++ PhzConfiguration/PhzConfiguration/PhotometryCatalogConfiguration.h
@@ -12,13 +12,54 @@
 namespace Euclid {
 namespace PhzConfiguration {
 
+/**
+ * @class PhotometryCatalogConfiguration
+ * 
+ * @brief Configuration class to be used by executables which want to have as
+ * input photometric catalogs
+ * 
+ * @details
+ * The catalog retrieved when using the getCatalog() method will contain sources
+ * with the PhotometryAttribute, populated as described by the parameters (see
+ * the getProgramOptions() documentation).
+ */
 class PhotometryCatalogConfiguration : virtual public CatalogConfiguration {
   
 public:
   
+  /**
+   * @brief Returns the program options required for photometric catalog input
+   * 
+   * @details
+   * These options include all the options defined by the CatalogConfiguration
+   * class, with the extra option \a filter-name-mapping. This option is a
+   * vector of strings with elements that do the mapping between the filter
+   * qualified names and the column names with the flux and error values,
+   * separated with spaces.
+   * 
+   * @return A boost::program_options::options_description object describing
+   * the program options
+   */
   static boost::program_options::options_description getProgramOptions();
   
-  PhotometryCatalogConfiguration(const std::map<std::string, boost::program_options::variable_value>& options);
+  /**
+   * @brief
+   * Constructs a new PhotometryCatalogConfiguration instance, for the given
+   * options
+   * 
+   * @details
+   * If no filter mapping is provided or if any of the filter mapping does not
+   * follow the format of three space separated strings an exception is thrown.
+   * 
+   * @param options
+   *    A map with the options and their values
+   * @throws ElementsException
+   *    if the given options do not contain any filter mapping
+   * @throws ElementsException
+   *    if any filter mapping does not follow the format "filter flux_name error_name"
+   */
+  PhotometryCatalogConfiguration(const std::map<std::string,
+                              boost::program_options::variable_value>& options);
   
 };
 
