104aa70ecf837deb6c0a36414328e1b9787f4a6e
Nikolaos Apostolakos <nikoapos@gmail.com>
2015-01-08 10:33:08 +0000

    Fixed indentations and removed unnecessary usages of the Euclid namespace
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Phosphoros/trunk@6665 463500fe-d6be-4184-9b20-47717e75ecac

--- PhzModeling/PhzModeling/ApplyFilterFunctor.h
+++ PhzModeling/PhzModeling/ApplyFilterFunctor.h
@@ -21,44 +21,44 @@ namespace XYDataset {
 namespace PhzModeling {
 
 /**
- * @class Euclid::PhzModeling::ApplyFilterFunctor
+ * @class PhzModeling::ApplyFilterFunctor
  * @brief
- * This functor is in charge of applying a filter to the Photometry model.
+ * This functor is in charge of applying a filter to a model dataset.
  * @details
- * This functor takes the Photometry model and multiply its components by
+ * This functor takes the model dataset and multiply its components by
  * the provided filter
  */
 class ApplyFilterFunctor {
 
 public:
 
-    /**
-        * @brief Function Call Operator
-        * @details
-        * Apply the filter to the Photometry Model.
-        * Points out of the range are discarded
-        * (we keep the first point before and after the range
-        * for interpolation purpose)
-        *
-        * @param model
-        * A XYDataset representing the Photometry Model to be filtered.
-        *
-        * @param filter_range
-        * A pair of double defining in which interval
-        * the filter assumes non zero values.
-        *
-        * @param filter
-        * A Function which takes a wavelength in input and returns
-        * the filter transmission (defined in interval [0,1])
-        *
-        * @return
-        * A XYDataset representing the filtered Photometry Model.
-        */
-    Euclid::XYDataset::XYDataset operator()(
-      const Euclid::XYDataset::XYDataset& model,
-      const std::pair<double,double>& filter_range,
-      const Euclid::MathUtils::Function& filter
-    ) const;
+  /**
+   * @brief Function Call Operator
+   * @details
+   * Apply the filter to the Model dataset.
+   * Points out of the range are discarded
+   * (we keep the first point before and after the range
+   * for interpolation purpose)
+   *
+   * @param model
+   * An XYDataset representing the Model to be filtered.
+   *
+   * @param filter_range
+   * A pair of double defining in which interval
+   * the filter assumes non zero values.
+   *
+   * @param filter
+   * A Function which takes a wavelength in input and returns
+   * the filter transmission (defined in interval [0,1])
+   *
+   * @return
+   * A XYDataset representing the filtered Model.
+   */
+  XYDataset::XYDataset operator()(
+    const XYDataset::XYDataset& model,
+    const std::pair<double,double>& filter_range,
+    const MathUtils::Function& filter
+  ) const;
 
 };
 
--- PhzModeling/src/lib/ApplyFilterFunctor.cpp
+++ PhzModeling/src/lib/ApplyFilterFunctor.cpp
@@ -11,46 +11,45 @@
 namespace Euclid {
 namespace PhzModeling {
 
-Euclid::XYDataset::XYDataset ApplyFilterFunctor::operator()(
-  const Euclid::XYDataset::XYDataset& model,
-  const std::pair<double,double>& filter_range,
-  const Euclid::MathUtils::Function& filter
-) const {
-    std::vector<std::pair<double, double>> filtered_values {};
-    bool has_entered_range=false;
-    bool is_first=true;
+XYDataset::XYDataset ApplyFilterFunctor::operator()(
+                              const XYDataset::XYDataset& model,
+                              const std::pair<double,double>& filter_range,
+                              const MathUtils::Function& filter) const {
+  std::vector<std::pair<double, double>> filtered_values {};
+  bool has_entered_range=false;
+  bool is_first=true;
 
-    for (auto model_iterator=model.begin();model_iterator!= model.end();++model_iterator) {
-      // before entering the range
-      if (filter_range.first>model_iterator->first){
-        is_first=false;
-        continue;
-      }
-
-      // first entrance get the previous point
-      if (!has_entered_range){
-        has_entered_range=true;
-        if (!is_first){
-          --model_iterator;
-          filtered_values.emplace_back(model_iterator->first, 0.);
-          ++model_iterator;
-        }
-      }
-
-      //after the range get the last point and break
-      if (filter_range.second<model_iterator->first){
-        filtered_values.emplace_back(model_iterator->first, 0.);
-        break;
-      }
-
-      // inside the range
-      filtered_values.emplace_back(
-          model_iterator->first,
-          model_iterator->second*filter(model_iterator->first)
-          );
+  for (auto model_iterator=model.begin();model_iterator!= model.end();++model_iterator) {
+    // before entering the range
+    if (filter_range.first>model_iterator->first){
+      is_first=false;
+      continue;
     }
 
-    return  Euclid::XYDataset::XYDataset {std::move(filtered_values)};
+    // first entrance get the previous point
+    if (!has_entered_range){
+      has_entered_range=true;
+      if (!is_first){
+        --model_iterator;
+        filtered_values.emplace_back(model_iterator->first, 0.);
+        ++model_iterator;
+      }
+    }
+
+    //after the range get the last point and break
+    if (filter_range.second<model_iterator->first){
+      filtered_values.emplace_back(model_iterator->first, 0.);
+      break;
+    }
+
+    // inside the range
+    filtered_values.emplace_back(
+        model_iterator->first,
+        model_iterator->second*filter(model_iterator->first)
+        );
+  }
+
+  return  XYDataset::XYDataset {std::move(filtered_values)};
 }
 
 } // end of namespace PhzModeling
