e64076143390b44083b33e88c582a15d34a81a85
Nikolaos Apostolakos <nikoapos@gmail.com>
2015-01-09 16:50:45 +0000

    Removed unnecessary namespace usages from the BuildFilterInfoFunctor.cpp
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Phosphoros/trunk@6688 463500fe-d6be-4184-9b20-47717e75ecac

--- PhzModeling/src/lib/BuildFilterInfoFunctor.cpp
+++ PhzModeling/src/lib/BuildFilterInfoFunctor.cpp
@@ -20,7 +20,7 @@ namespace PhzModeling {
  * the filter has a zero value and ends to the last value of lambda for which
  * the filter value is again 0
  */
-std::pair<double,double> getRange(const Euclid::XYDataset::XYDataset& filter_dataset) {
+std::pair<double,double> getRange(const XYDataset::XYDataset& filter_dataset) {
   size_t min = 0;
   size_t current = 0;
   size_t max = 0;
@@ -51,26 +51,26 @@ std::pair<double,double> getRange(const Euclid::XYDataset::XYDataset& filter_dat
  * take the sampling, multiply it by 1/lambda², then take a linear interpolation
  * and return c * the integral.
  */
-double computeNormalization(const Euclid::XYDataset::XYDataset& filter_dataset, std::pair<double,double> range)  {
+double computeNormalization(const XYDataset::XYDataset& filter_dataset, std::pair<double,double> range)  {
   std::vector<std::pair<double, double>> normalized_values {};
   for (auto& sed_pair : filter_dataset) {
     normalized_values.push_back(std::make_pair(sed_pair.first,sed_pair.second/(sed_pair.first*sed_pair.first)));
   }
-  auto filter_over_lambda_squar=Euclid::MathUtils::interpolate(normalized_values,Euclid::MathUtils::InterpolationType::LINEAR);
-  auto integral_value = Euclid::MathUtils::integrate(*filter_over_lambda_squar,range.first,range.second);
+  auto filter_over_lambda_squar=MathUtils::interpolate(normalized_values,MathUtils::InterpolationType::LINEAR);
+  auto integral_value = MathUtils::integrate(*filter_over_lambda_squar,range.first,range.second);
   return integral_value * Elements::Units::c_light/ Elements::Units::angstrom;
 }
 
 // create a function as a linear interpolation of the provided filter sampling
-std::unique_ptr<Euclid::MathUtils::Function> computeFunction(const Euclid::XYDataset::XYDataset& filter_dataset) {
-  return Euclid::MathUtils::interpolate(filter_dataset,Euclid::MathUtils::InterpolationType::LINEAR);
+std::unique_ptr<MathUtils::Function> computeFunction(const XYDataset::XYDataset& filter_dataset) {
+  return MathUtils::interpolate(filter_dataset,MathUtils::InterpolationType::LINEAR);
 }
 
-Euclid::PhzDataModel::FilterInfo BuildFilterInfoFunctor::operator()(const Euclid::XYDataset::XYDataset& filter_dataset) const {
+PhzDataModel::FilterInfo BuildFilterInfoFunctor::operator()(const XYDataset::XYDataset& filter_dataset) const {
    auto range =getRange(filter_dataset);
    double normalization = computeNormalization(filter_dataset, range);
    auto function_ptr=computeFunction(filter_dataset);
-   return Euclid::PhzDataModel::FilterInfo(range,*function_ptr,normalization);
+   return PhzDataModel::FilterInfo(range,*function_ptr,normalization);
 }
 
 } // end of namespace PhzModeling
