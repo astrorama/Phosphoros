d066caccba4c9a270bb08b88d1eac6c59175eed1
Nikolaos Apostolakos <nikoapos@gmail.com>
2015-11-16 17:29:34 +0000

    Updated the GUI to use the new configuration classes when creating the models photometry grid
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Phosphoros/branches/Luminosity@10151 463500fe-d6be-4184-9b20-47717e75ecac

--- PhzQtUI/src/lib/DialogGridGeneration.cpp
+++ PhzQtUI/src/lib/DialogGridGeneration.cpp
@@ -11,10 +11,20 @@
 #include "PhzQtUI/DialogGridGeneration.h"
 #include <boost/program_options.hpp>
 #include "ui_DialogGridGeneration.h"
-#include "PhzConfiguration/ComputeModelGridConfiguration.h"
+#include "PhzConfiguration/ComputeModelGridConfig.h"
+#include "PhzConfiguration/SedProviderConfig.h"
+#include "PhzConfiguration/ReddeningProviderConfig.h"
+#include "PhzConfiguration/FilterProviderConfig.h"
+#include "PhzConfiguration/IgmConfig.h"
+#include "PhzConfiguration/ParameterSpaceConfig.h"
+#include "PhzConfiguration/FilterConfig.h"
+#include "PhzConfiguration/ModelGridOutputConfig.h"
 #include "PhzModeling/SparseGridCreator.h"
+#include "DefaultOptionsCompleter.h"
 
 // #include <future>
+
+using namespace Euclid::PhzConfiguration;
 
 namespace Euclid {
 namespace PhzQtUI {
@@ -50,20 +60,32 @@ void DialogGridGeneration::updateGridProgressBar(size_t step, size_t total) {
 
 std::string DialogGridGeneration::runFunction() {
   try {
-    PhzConfiguration::ComputeModelGridConfiguration conf { m_config };
-    PhzModeling::SparseGridCreator creator { conf.getSedDatasetProvider(),
-        conf.getReddeningDatasetProvider(), conf.getFilterDatasetProvider(),
-        conf.getIgmAbsorptionFunction() };
-
+    completeWithDefaults<ComputeModelGridConfig>(m_config);
+    long config_manager_id = std::chrono::duration_cast<std::chrono::microseconds>(
+                                    std::chrono::system_clock::now().time_since_epoch()).count();
+    auto& config_manager = Configuration::ConfigManager::getInstance(config_manager_id);
+    config_manager.registerConfiguration<ComputeModelGridConfig>();
+    config_manager.closeRegistration();
+    config_manager.initialize(m_config);
+    
+    auto& sed_provider = config_manager.getConfiguration<SedProviderConfig>().getSedDatasetProvider();
+    auto& reddening_provider = config_manager.getConfiguration<ReddeningProviderConfig>().getReddeningDatasetProvider();
+    const auto& filter_provider = config_manager.getConfiguration<FilterProviderConfig>().getFilterDatasetProvider();
+    auto& igm_abs_func = config_manager.getConfiguration<IgmConfig>().getIgmAbsorptionFunction();
+    
+    Euclid::PhzModeling::SparseGridCreator creator {
+                sed_provider, reddening_provider, filter_provider, igm_abs_func};
+    
     std::function<void(size_t, size_t)> monitor_function = std::bind(
         &DialogGridGeneration::updateGridProgressBar, this,
         std::placeholders::_1, std::placeholders::_2);
 
-    auto result = creator.createGrid(conf.getParameterSpaceRegions(),
-                                     conf.getFilterList(),
-                                     monitor_function);
+    auto param_space_map = config_manager.getConfiguration<ParameterSpaceConfig>().getParameterSpaceRegions();
+    auto filter_list = config_manager.getConfiguration<FilterConfig>().getFilterList();
+    
+    auto result = creator.createGrid(param_space_map, filter_list, monitor_function);
 
-    auto output = conf.getOutputFunction();
+    auto output = config_manager.getConfiguration<ModelGridOutputConfig>().getOutputFunction();
     output(result);
     return "";
   }
