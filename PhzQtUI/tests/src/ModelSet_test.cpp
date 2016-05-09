1d89b0e29d92881b0e92deacc1ff370b0f0e3238
Florian Dubath <Florian.Dubath@unige.ch>
2016-05-09 14:42:50 +0000

    update tests
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/Phosphoros/trunk@13477 463500fe-d6be-4184-9b20-47717e75ecac

--- PhzQtUI/tests/src/ModelSet_test.cpp
+++ PhzQtUI/tests/src/ModelSet_test.cpp
@@ -9,7 +9,7 @@
 #include "ElementsKernel/Real.h" // isEqual
 
 using namespace Euclid::PhzQtUI;
-/*
+
 struct ModelSet_Fixture {
   std::string ref_name = "Parameter_space_config_name";
   std::string ref_rule_1_name = "Rule_1";
@@ -17,19 +17,32 @@ struct ModelSet_Fixture {
   ParameterRule ref_rule_1;
   Range ref_z_range = Range { 0.1, 5.5, 0.2 };
   Range ref_ebv_range = Range { 1., 3.5, 0.5 };
-  std::string ref_sed_root = "group1/sed_root";
-  std::string ref_red_root = "group2/red_root";
-  std::vector<std::string> ref_excl_sed { "EX_SED_1", "EX_SED_2" };
-  std::vector<std::string> ref_excl_red { "EX_RED_1", "EX_RED_2", "EX_RED_3" };
+
+
+  std::set<double> ref_z_values{0,6};
+  std::set<double> ref_ebv_values{0,1,3,5,10};
+
+  DatasetSelection ref_red_selection{};
+  DatasetSelection ref_sed_selection{};
 
   ModelSet_Fixture() {
+
+    ref_red_selection.setGroupes({"red_group1"});
+    ref_red_selection.setIsolated({"isolated_red_1","isolated_red_2"});
+    ref_red_selection.setExclusions({"red_group1/red_excluded_1"});
+
+    ref_sed_selection.setGroupes({"sed_group1","sed_group2","sed_group3"});
+    ref_sed_selection.setIsolated({"isolated_sed_1","isolated_sed_2"});
+    ref_sed_selection.setExclusions({"sed_group1/sed_excluded_1"});
+
     ref_rule_1.setName(ref_rule_1_name);
-    ref_rule_1.setZRange(ref_z_range);
-    ref_rule_1.setEbvRange(ref_ebv_range);
-    ref_rule_1.setSedRootObject(ref_sed_root);
-    ref_rule_1.setReddeningRootObject(ref_red_root);
-    ref_rule_1.setExcludedSeds(ref_excl_sed);
-    ref_rule_1.setExcludedReddenings(ref_excl_red);
+    ref_rule_1.setZRanges({ref_z_range});
+    ref_rule_1.setEbvRanges({ref_ebv_range});
+
+    ref_rule_1.setEbvValues(ref_ebv_values);
+    ref_rule_1.setRedshiftValues(ref_z_values);
+    ref_rule_1.setRedCurveSelection(ref_red_selection);
+    ref_rule_1.setSedSelection(ref_sed_selection);
 
   }
 
@@ -64,61 +77,86 @@ BOOST_FIXTURE_TEST_CASE(serialize_test, ModelSet_Fixture) {
   auto rules_node = root_node.firstChildElement("ParameterRules");
   auto rule_element = rules_node.childNodes().at(0).toElement();
 
-  BOOST_CHECK_EQUAL(rule_element.attribute("Name").toStdString(),
-      ref_rule_1_name);
-  BOOST_CHECK_EQUAL(rule_element.attribute("SedRootObject").toStdString(),
-      ref_sed_root);
-  BOOST_CHECK_EQUAL(
-      rule_element.attribute("ReddeningCurveRootObject").toStdString(),
-      ref_red_root);
+  BOOST_CHECK_EQUAL(rule_element.attribute("Name").toStdString(), ref_rule_1_name);
 
-  auto z_node = rule_element.firstChildElement("ZRange");
-  BOOST_CHECK(Elements::isEqual(z_node.attribute("Min").toDouble(), 0.1));
-  BOOST_CHECK(Elements::isEqual(z_node.attribute("Max").toDouble(), 5.5));
-  BOOST_CHECK(Elements::isEqual(z_node.attribute("Step").toDouble(), 0.2));
 
-  auto ebv_node = rule_element.firstChildElement("EbvRange");
-  BOOST_CHECK(Elements::isEqual(ebv_node.attribute("Min").toDouble(), 1.));
-  BOOST_CHECK(Elements::isEqual(ebv_node.attribute("Max").toDouble(), 3.5));
-  BOOST_CHECK(Elements::isEqual(ebv_node.attribute("Step").toDouble(), 0.5));
+  auto z_range_node_list = rule_element.firstChildElement("ZRanges").childNodes();
+  for (int j = 0; j < z_range_node_list.count(); ++j) {
+          auto range_node = z_range_node_list.at(j).toElement();
+          BOOST_CHECK(Elements::isEqual(range_node.attribute("Min").toDouble(), 0.1));
+          BOOST_CHECK(Elements::isEqual(range_node.attribute("Max").toDouble(), 5.5));
+          BOOST_CHECK(Elements::isEqual(range_node.attribute("Step").toDouble(), 0.2));
 
-  auto excl_sed_list =
-      rule_element.firstChildElement("ExcludedSeds").childNodes();
-  for (int j = 0; j < excl_sed_list.count(); ++j) {
-    auto sub_node = excl_sed_list.at(j).toElement();
-    if (sub_node.hasChildNodes()) {
-      bool found = false;
-      std::string txt = sub_node.text().toStdString();
-      for (auto& excl : ref_excl_sed) {
-        if (txt.compare(excl) == 0) {
-          found = true;
-        }
-      }
-
-      BOOST_CHECK(found);
-
-    }
   }
 
-  auto excl_red_list =
-      rule_element.firstChildElement("ExcludedReddeningCurves").childNodes();
-  for (int j = 0; j < excl_red_list.count(); ++j) {
-    auto sub_node = excl_red_list.at(j).toElement();
-    if (sub_node.hasChildNodes()) {
-      bool found = false;
-      std::string txt = sub_node.text().toStdString();
-      for (auto& excl : ref_excl_red) {
-        if (txt.compare(excl) == 0) {
-          found = true;
-        }
+  auto ebv_range_node_list = rule_element.firstChildElement("EbvRanges").childNodes();
+   for (int j = 0; j < ebv_range_node_list.count(); ++j) {
+           auto range_node = ebv_range_node_list.at(j).toElement();
+           BOOST_CHECK(Elements::isEqual(range_node.attribute("Min").toDouble(), 1.));
+           BOOST_CHECK(Elements::isEqual(range_node.attribute("Max").toDouble(), 3.5));
+           BOOST_CHECK(Elements::isEqual(range_node.attribute("Step").toDouble(), 0.5));
+
+   }
+
+   std::set<double> z_value_set { };
+           auto z_sub_list = rule_element.firstChildElement("ZValues").childNodes();
+           for (int j = 0; j < z_sub_list.count(); ++j) {
+             auto sub_node = z_sub_list.at(j).toElement();
+             if (sub_node.hasChildNodes()) {
+               z_value_set.insert(sub_node.text().toDouble());
+             }
+           }
+
+      BOOST_CHECK_EQUAL(z_value_set.size(),ref_z_values.size());
+      for(auto& value : z_value_set){
+        BOOST_CHECK(ref_z_values.find(value)!=ref_z_values.end());
       }
 
-      BOOST_CHECK(found);
-    }
-  }
+   std::set<double> ebv_value_set { };
+        auto ebv_sub_list = rule_element.firstChildElement("EbvValues").childNodes();
+        for (int j = 0; j < ebv_sub_list.count(); ++j) {
+          auto sub_node = ebv_sub_list.at(j).toElement();
+          if (sub_node.hasChildNodes()) {
+            ebv_value_set.insert(sub_node.text().toDouble());
+          }
+        }
+
+   BOOST_CHECK_EQUAL(ebv_value_set.size(),ref_ebv_values.size());
+   for(auto& value : ebv_value_set){
+     BOOST_CHECK(ref_ebv_values.find(value)!=ref_ebv_values.end());
+   }
+
+   auto sed_node_list = rule_element.firstChildElement("SedsSelection");
+   auto group_nodes = sed_node_list.firstChildElement("SedGroups").childNodes();
+   BOOST_CHECK_EQUAL(group_nodes.count(),ref_sed_selection.getGroupes().size());
+   BOOST_CHECK_EQUAL(group_nodes.at(0).toElement().text().toStdString(),ref_sed_selection.getGroupes()[0]);
+
+   auto isolated_nodes = sed_node_list.firstChildElement("SedIsolated").childNodes();
+   BOOST_CHECK_EQUAL(isolated_nodes.count(),ref_sed_selection.getIsolated().size());
+   BOOST_CHECK_EQUAL(isolated_nodes.at(0).toElement().text().toStdString(),ref_sed_selection.getIsolated()[0]);
+
+   auto excluded_nodes = sed_node_list.firstChildElement("SedExcluded").childNodes();
+   BOOST_CHECK_EQUAL(excluded_nodes.count(),ref_sed_selection.getExclusions().size());
+   BOOST_CHECK_EQUAL(excluded_nodes.at(0).toElement().text().toStdString(),ref_sed_selection.getExclusions()[0]);
+
+
+
+   auto red_node_list = rule_element.firstChildElement("RedCurvesSelection");
+   group_nodes = red_node_list.firstChildElement("RedCurveGroups").childNodes();
+   BOOST_CHECK_EQUAL(group_nodes.count(),ref_red_selection.getGroupes().size());
+   BOOST_CHECK_EQUAL(group_nodes.at(0).toElement().text().toStdString(),ref_red_selection.getGroupes()[0]);
+
+   isolated_nodes = red_node_list.firstChildElement("RedCurveIsolated").childNodes();
+   BOOST_CHECK_EQUAL(isolated_nodes.count(),ref_red_selection.getIsolated().size());
+   BOOST_CHECK_EQUAL(isolated_nodes.at(0).toElement().text().toStdString(),ref_red_selection.getIsolated()[0]);
+
+   excluded_nodes = red_node_list.firstChildElement("RedCurveExcluded").childNodes();
+   BOOST_CHECK_EQUAL(excluded_nodes.count(),ref_red_selection.getExclusions().size());
+   BOOST_CHECK_EQUAL(excluded_nodes.at(0).toElement().text().toStdString(),ref_red_selection.getExclusions()[0]);
+
 
 }
 
 // Ends the test suite
 BOOST_AUTO_TEST_SUITE_END ()
-*/
+
