ed403d880bb726bc4193c8c134368260923a33a5
Nicolas Morisset <Nicolas.Morisset@unige.ch>
2018-11-07 10:06:01 +0100

    Implementation of AddGalDustToCatalog.py script

--- GalacticDustMap/python/GalacticDustMap/AddGalDustToCatalog.py
+++ GalacticDustMap/python/GalacticDustMap/AddGalDustToCatalog.py
@@ -30,6 +30,10 @@ if sys.version_info[0] < 3:
     from future_builtins import *
 
 import argparse
+from astropy.table import Table
+import ElementsKernel.Logging as log
+import GalacticDustMap
+import ElementsKernel.Auxiliary as aux
 import ElementsKernel.Logging as log
 
 def defineSpecificProgramOptions():
@@ -47,8 +51,16 @@ def defineSpecificProgramOptions():
 
     #
     # !!! Write your program options here !!!
-    # e.g. parser.add_argument('--string-value', type=str, help='A string option')
-    #
+    parser.add_argument('--planck-dust-map', type=str, help='Filename of the Planck dust map in HEALPIX format',
+                        default='')
+    parser.add_argument('--input-catalog', type=str, help='Input catalog filename')
+    parser.add_argument('--output-catalog', type=str, help='Output catalog filename')
+    parser.add_argument('--ra', type=str, help='Right Acsension column in the catalog (Degrees)')
+    parser.add_argument('--dec', type=str, help='Declination column in the catalog (Degres)')
+    parser.add_argument('--galatic-ebv-col', type=str, 
+                        help='Name of the column to be added to the output catalog',
+                        default='GAL_EBV')
+
 
     return parser
 
@@ -67,12 +79,26 @@ def mainMethod(args):
     logger.info('# Entering AddGalDustToCatalog mainMethod()')
     logger.info('#')
 
-    # !! Getting the option from the example option in defineSpecificProgramOption
-    # !! e.g string_option = args.string_value
+    # Read the Planck map
+    if arg.planck_dust_map:
+        aux_file = arg.planck_dust_map
+    else:
+        aux_file = aux.getAuxiliaryPath('PlanckEvb.fits')
+    map_data = loadMap(aux_file)
 
-    #
-    # !!! Write you main code here !!!
-    #
+    # open catalog
+    input_cat = Table.read(args.input_catalog)
+    if not args.ra in input_cat.colnames:
+        raise ValueError("RA column  missing : %s " % args.ra)
+    if not args.dec in input_cat.colnames:
+        raise ValueError("DEC column  missing : %s " % args.dec)
+
+    ra = input_cat[args.ra]
+    dec = input_cat[args.dec]
+
+    ebv = ebv_planck(map_data, ra, dec)
+    input_cat[args.galatic_ebv_col] = ebv
+    input_catalog.write(args.output_catalog)
 
     logger.info('#')
     logger.info('# Exiting AddGalDustToCatalog mainMethod()')
