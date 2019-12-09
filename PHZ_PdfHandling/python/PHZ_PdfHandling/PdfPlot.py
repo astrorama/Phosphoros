7e7f68e11c32f278b19610bf0fca2ff97ce89c1f
Florian Dubath <florian.dubath@unige.ch>
2019-12-09 11:57:18 +0100

    Program to plot stacked PDF and PIT/CRPS

---
+++ PHZ_PdfHandling/python/PHZ_PdfHandling/PdfPlot.py
@@ -0,0 +1,267 @@
+#!/usr/bin/env python
+
+"""
+Guillaume Desprez - 2019 - (guillaume.desprez@unige.ch)
+Script to plot PDZ versus spec-z
+"""
+import numpy as np
+import scipy.integrate as integrate
+from scipy.optimize import curve_fit
+
+
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+
+# Point estimate determination
+
+def getMaxSamplingPointEstimate(pdf_bins, pdf_data):
+    """For each  object, return the value of the sampling for which the 1D-PDF has the higgest value
+    
+    Parameters:
+    
+    pdf_bins (array): Sampling of the 1D-PDF
+    pdf_data (array(array)): an array (1 element by object) of 1-PDF values (evaluated at the 'pdf-bins' point)
+    
+    Returns:
+    
+    array: list of value of the sampling for wich the object's 1D-PDF has the higgest value
+    
+    """
+    return [pdf_bins[np.argmax(pdf)] for pdf in pdf_data]
+    
+def getMeanPointEstimate(pdf_bins, pdf_data):
+    """For each  object, return the mean value of the 1D-PDF
+    
+    Parameters:
+    
+    pdf_bins (array): Sampling of the 1D-PDF
+    pdf_data (array(array)): an array (1 element by object) of 1-PDF values (evaluated at the 'pdf-bins' point)
+    
+    Returns:
+    
+    array: list of mean value (in the sampling axis) of the object's 1D-PDF
+    
+    """
+    return integrate.simps(np.multiply(pdf_bins,pdf_data), pdf_bins)/integrate.simps(pdf_data,  pdf_bins)
+    
+def quad(x, a, b, c):
+    return a*x**2 + b*x + c
+
+def getFitModeEstimate(pdf_bins, pdf_data, threshold = 0.7):
+    """For each  object, fit a parabola around the highest value of the 1D-PDF and retrun the hoggest point of the curve
+    
+    Parameters:
+    
+    pdf_bins (array): Sampling of the 1D-PDF
+    pdf_data (array(array)): an array (1 element by object) of 1-PDF values (evaluated at the 'pdf-bins' point)
+    threshold (float): selection of the reange on which the parabola is fitted: if set to 1 take only th highest point, to 0 all the sampling
+    
+    Returns:
+    
+    array: list of center of the fitted parabola 
+    
+    """
+    values = [pdf_bins[np.argmax(pdf)] for pdf in pdf_data]
+    for index in range(len(pdf_data)):
+        id_center = np.argwhere(pdf_bins==values[index])[0][0]
+        id_min = id_center
+        treshold_value = threshold*pdf_data[index][id_center]  
+        while id_min>0 and pdf_data[index][id_min]>treshold_value:
+            id_min = id_min - 1
+        id_max = id_center
+        while id_max<len(pdf_bins)-1 and pdf_data[index][id_max]>treshold_value:
+            id_max = id_max + 1
+
+        if id_max - id_min > 1:
+            fit = curve_fit(quad, pdf_bins[id_min:id_max], pdf_data[index][id_min:id_max])
+            if (fit[0][0]<0):
+                values[index] = -fit[0][1]/(2*fit[0][0])   
+
+    return values
+        
+# Stacked PDF 
+
+def _getShiftedPdfBins(pdf_bins):
+    minus_bins = -1.*pdf_bins
+    minus_bins.sort()
+    if 0 in pdf_bins:
+        full_bins = np.concatenate((minus_bins[:-1],pdf_bins))
+    else :
+        full_bins = np.concatenate((minus_bins,[0],pdf_bins))
+    
+    extended_bins = np.concatenate((full_bins,[2*full_bins[-1]-full_bins[-2]]))
+    return full_bins, extended_bins
+    
+def stackPdfFromBinInRef(pdf_data, reference_values, stack_bins):
+    """Stack 1D-PDF in bins. Each object is attributed to a bin based on its reference value 
+    
+    Parameters:
+    
+    pdf_data (array(array)): an array (1 element by object) of 1-PDF values 
+    reference_values (array): for each object the 'True' value of the PDF parameter
+    stack_bins(array): The bin into which the objects are sorted.
+    
+    Returns:
+    
+    2D-array: stacked PDF for each bins
+    
+    """
+    stack_bins = np.array(stack_bins)
+    
+    ref_map = np.zeros([len(pdf_data[0]),len(stack_bins)-1])
+    for index, ref_value in enumerate(reference_values):
+        index_bellow = stack_bins[stack_bins<ref_value] 
+        if len(index_bellow)>0 and len(index_bellow)<len(stack_bins):
+            ref_map[:,len(index_bellow)-1] += np.nan_to_num(pdf_data[index]/np.sum(pdf_data[index]))
+    return ref_map    
+    
+def stackedShiftedPdf(pdf_data, pdf_bins, point_estimates, reference_values, stack_bins):
+    """Stack 1D-PDF in bins. Each object is attributed to a bin based on its point-estimate value, PDFs are shifted to be centered on the reference value 
+    
+    Parameters:
+    
+    pdf_data (array(array)): an array (1 element by object) of 1-PDF values (evaluated at the 'pdf-bins' point)
+    pdf_bins (array): Sampling of the 1D-PDF
+    point_estimates (array): for each object the value of the parameter estimated from the PDF
+    reference_values (array): for each object the 'True' value of the PDF parameter
+    stack_bins(array): The bin into which the objects are sorted.
+    
+    Returns:
+    
+    2D-array: stacked PDF for each bins
+    
+    """
+    full_bins, extended_bins = _getShiftedPdfBins(pdf_bins)
+    added_nodes = int(len(full_bins)-len(pdf_bins))
+    stack_bins = np.array(stack_bins)
+    
+    shift_map = np.zeros([len(full_bins),len(stack_bins)-1])
+    for index, point_estimate in enumerate(point_estimates):
+        index_bellow = stack_bins[stack_bins<point_estimate]
+        if len(index_bellow)>0 and len(index_bellow)<len(stack_bins):
+            # Force the PDF to 0 outside of the original sampling
+            completed_pdf = np.concatenate((np.zeros(added_nodes), pdf_data[index]/np.sum(pdf_data[index]),[0]))
+            shifted_pdf = np.interp(full_bins, extended_bins-reference_values[index], completed_pdf)
+            shift_map[:,len(index_bellow)-1] += np.nan_to_num(shifted_pdf)
+    return shift_map, full_bins   
+    
+# Confidence interval on stacked PDF   
+def getInterval(data_map, pdf_map_bins, fraction):
+    """Compute, for the stacked PDFs grid, median centered intervals containing a fraction of object
+    
+    Parameters:
+    data_map (2D-array): stacked 1D-PDF for a set of bins
+    pdf_map_bins (array): sampling of the stacked PDF axis
+    fraction (float): the ratio of object to be into the computed interval
+    
+    Returns:
+    
+    array(array): for each bins an array containing the min and max of the interval
+    
+    """
+    cumul = np.cumsum(data_map, axis=0)
+    totals = np.sum(data_map, axis=0)
+    
+    interval_lim = np.zeros((data_map.shape[1],2))
+    for index in range(data_map.shape[1]):
+        if totals[index] == 0:
+            interval_lim[index] = [0,0]
+        else:
+            interval_lim[index] = [np.interp(0.5*(1.-fraction)*totals[index], cumul[:,index], pdf_map_bins), 
+                                   np.interp((0.5+0.5*fraction)*totals[index], cumul[:,index], pdf_map_bins)]   
+    return interval_lim
+
+# Probability Integral Transform (PIT) and Continuous Ranked Probabilty score (CRPS)    
+def computePitAndCrps(pdf_data, pdf_bins, reference_values, progress_callback=None):
+    """Compute the Probability Integral Transform (PIT) and Continuous Ranked Probabilty score (CRPS) for objects
+    
+    Parameters:
+    
+    pdf_data (array(array)): an array (1 element by object) of 1-PDF values(evaluated at the 'pdf-bins' point)
+    pdf_bins (array): Sampling of the 1D-PDF 
+    reference_values (array): for each object the 'True' value of the PDF parameter
+    
+    Returns:
+    array, array: list of PIT, list of CRPS
+    
+    """
+    total = len(reference_values)
+    pits = np.zeros(total)
+    crps = np.zeros(total)
+    for index, ref_value in enumerate(reference_values):
+        current_bin = np.array(pdf_bins)
+        current_data = np.array(pdf_data[index])
+        current_cumul_data =  [integrate.simps(pdf_data[index][:i], pdf_bins[:i]) if i>0 else 0 for i in range(len(pdf_bins))] / integrate.simps(pdf_data[index], pdf_bins)
+        if not ref_value in current_bin:
+            current_bin = np.concatenate((current_bin,[ref_value]))
+            current_bin.sort()
+            current_data = np.interp(current_bin, pdf_bins, current_data)
+            current_cumul_data = np.interp(current_bin, pdf_bins, current_cumul_data)
+        current_data = current_data / integrate.simps(current_data, current_bin)
+        index_cut = np.argwhere(current_bin==ref_value)[0][0]
+        if index_cut > 0:
+            pits[index] = integrate.simps(current_data[:index_cut], current_bin[:index_cut]) 
+            if index_cut < len(current_bin)-1:
+                crps[index] = integrate.simps(np.square(current_cumul_data[:index_cut]), current_bin[:index_cut]) + integrate.simps(np.square(current_cumul_data[index_cut:] -1), current_bin[index_cut:])
+            else:
+                crps[index] = integrate.simps(np.square(current_cumul_data), current_bin) 
+        else:
+            crps[index] =  integrate.simps(np.square(current_cumul_data -1), current_bin)
+        if progress_callback is not None:
+            progress_callback(index,total)
+    return pits, crps    
+        
+# Ploting tools
+def plotPdfMap(data_map, pdf_map_bins, stack_bins, fractions, colors = ['r','darkorange','orange','yellow','lightgrey'], **kwargs):
+    """Plot stacked PDF with confidence intervals 
+    
+    Parameters:
+    
+    data_map (2D-array): stacked 1D-PDF for a set of bins
+    pdf_map_bins (array): sampling of the stacked PDF axis
+    stack_bins(array): The bin into which the objects are sorted.
+    fractions (list of float): List of values for which the confidence intervals have to be plotted 
+    colors (list of color): a list of color to be used for the confidence interval plots. If the 'fraction' list is longer than 'colors' the last colors is used multiple-time  
+
+    """
+    cs = plt.imshow(data_map,origin='lower',
+                    extent=(0,stack_bins[-1], pdf_map_bins[0],pdf_map_bins[-1]),
+                    aspect='auto',**kwargs)
+    
+    plt.colorbar(cs)
+     
+    color_index = 0
+    for frac in fractions:
+        interval = getInterval(data_map, pdf_map_bins, frac) 
+        plt.plot(stack_bins,np.append(interval[:,0],interval[-1,0]) ,c=colors[color_index],linewidth=1,drawstyle='steps-post',alpha=0.8,label=str(int(frac*100))+'% Lim')
+        plt.plot(stack_bins,np.append(interval[:,1],interval[-1,1]),c=colors[color_index],linewidth=1,drawstyle='steps-post',alpha=0.8)
+        if color_index<len(colors) - 1:
+            color_index = color_index + 1
+    plt.legend()  
+    
+def plotPIT( pits, histo_bins=20,**kwargs):
+    """Plot an histogram of the PIT values 
+    
+    Parameters:
+    
+    pits (array): set of PIT value
+    histo_bins(int): Number of bins for the histogram
+
+    """
+    mean = 1
+    plt.title("PDF's PIT")
+    plt.hist(pits,bins=histo_bins,range=(0,1),density=True,**kwargs)
+    plt.plot([0,1],[mean,mean])
+
+def plotCRPS( crps, histo_bins=20,**kwargs):
+    """Plot an histogram of the CRPS values 
+    
+    Parameters:
+    
+    crps (array): set of PIT value
+    histo_bins(int): Number of bins for the histogram
+
+    """
+    plt.title("PDF's CRPS")
+    plt.hist(crps,bins=histo_bins,range=(0,crps.max()),density=True,**kwargs)
+            
---
+++ PHZ_PdfHandling/python/PHZ_PdfHandling/PlotStackedPdfPitAndCRPS.py
@@ -0,0 +1,149 @@
+
+import argparse
+from ElementsKernel import Logging
+from ElementsKernel.Program import str_to_bool
+from ElementsKernel import Exit
+
+import numpy as np
+
+from astropy.io import fits
+import re
+from astropy import table
+
+import matplotlib.pyplot as plt
+import PHZ_PdfHandling.PdfPlot
+from matplotlib.colors import LogNorm
+
+
+def defineSpecificProgramOptions():
+    """
+    @brief Allows to define the (command line and configuration file) options specific to
+    this program
+
+    @details
+        See the ElementsProgram documentation for more details.
+    @return
+        An  ArgumentParser.
+    """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--pdz-catalog-file', type=str, help='Path to the .fits file containing the PDZ and the point estimate')
+    parser.add_argument('--refz-catalog-file', type=str, default='', help='Path to the .fits file containing the reference redshift ( If not provided the program looks for the refZ into pdz-catalog-file)')
+    parser.add_argument('--pdz-col-id', type=str, default='ID', help='Column containing the identifiant of the source in the pdz-catalog-file')
+    parser.add_argument('--pdz-col-pdf', type=str, default='Z-1D-PDF', help='Column containing the PDZ in the pdz-catalog-file')
+    parser.add_argument('--pdz-col-pe', type=str, default='Z', help='Column containing the point estimate of the redshift in the pdz-catalog-file')
+    parser.add_argument('--refz-col-id', type=str, default='ID', help='Column containing the identifiant of the source in the refz-catalog-file (Not used if refz-catalog-file is skipped or identical to pdz-catalog-file)')
+    parser.add_argument('--refz-col-ref', type=str, default='Z-TRUE', help='Column containing the reference Redshift of the source in the refz-catalog-file')
+    parser.add_argument('--stack-bins', type=int, default='20', help='Number of bin for the stacking of the pdf')
+    parser.add_argument('--hist-bins', type=int, default='20', help='Number of bin for the histograms')
+    parser.add_argument('--ref-plot', type=str, default='True', help='Display the stacked PDF VS reference plot (default "True")')
+    parser.add_argument('--shift-plot', type=str, default='True', help='Display the shifted and stacked PDF plot (default "True")')
+    parser.add_argument('--pit-plot', type=str, default='True', help='Display the pit plot (computation of PIT and CRPS may ) (default "True")')
+    parser.add_argument('--crps-plot', type=str, default='True', help='Display the crps plot (computation of PIT and CRPS may ) (default "True")')
+    
+    return parser
+ 
+def getFitsComment(file_path):
+    hdul = fits.open(file_path)    
+    comment =  str(hdul[1].header['COMMENT'])
+
+    hdul.close();
+    return comment
+ 
+def readPdzSampling(comment):
+    reg = "Z-BINS:\{[\d,.Ee-]+"
+    if "LIKELIHOOD-Z-BINS" in comment:
+        reg = "LIKELIHOOD-"+reg
+    data = re.findall(reg,comment.replace('\n','').replace(' ',''))
+    if len(data)!=1:
+        raise Exception('Unable to find sampling in the file.')
+    nodes = data[0].replace('\n','').replace(' ','').split('{')[1].split(',')
+    pdz_bins = [float(z) for z in nodes]
+    return pdz_bins
+
+
+def readTable(file_path, columns, index =1):
+    hdul = fits.open(file_path) 
+    checkColumns(hdul[index], columns)
+    data = hdul[index].data
+    return data  
+  
+def checkColumns(hdu, columns):
+    for col in columns:
+        if col not in hdu.columns.names:
+           raise Exception('The Column '+col+' is missing in the fits file.') 
+
+def progressCallback(value,total):
+    ten = int(total/20)
+    if value%ten==0:
+        logger = Logging.getLogger('PlotStackedPdfPitAndCrps')
+        logger.info('Computing PIT & CRPS:'+str(int(100*value/total))+"%")
+        
+            
+
+def mainMethod(args):
+    """
+    @brief The "main" method.
+    @details This method is the entry point to the program. In this sense, it is similar to a main
+    (and it is why it is called mainMethod()). The code below contains the calls to the
+    different classes created for the first developer's workshop
+
+        See the ElementsProgram documentation for more details.
+    """
+    logger = Logging.getLogger('PlotStackedPdfPitAndCrps')
+    logger.info('Entering PlotStackedPdfPitAndCrps mainMethod()')
+    
+    columns_in_pdz = [args.pdz_col_id, args.pdz_col_pdf, args.pdz_col_pe]
+    column_in_refz = [args.refz_col_id, args.refz_col_ref]
+    
+    two_files = True
+    if args.refz_catalog_file == '' or args.refz_catalog_file == args.pdz_catalog_file:
+        columns_in_pdz.append(args.refz_col_ref) 
+        two_files = False
+    
+    # Get the PDZ sampling
+    pdz_bins = np.array(readPdzSampling(getFitsComment(args.pdz_catalog_file))) 
+    nb_bin = args.stack_bins
+    stack_bins= [ pdz_bins[0] + (pdz_bins[-1]+pdz_bins[0])/(1.0*nb_bin)*index for index in range(nb_bin+1)]
+    logger.info('Bins borders for the stacking of the PDF :' + str(stack_bins))
+
+    #get the data
+    data = readTable(args.pdz_catalog_file, columns_in_pdz)
+    
+    pdf_data = data[:][args.pdz_col_pdf]
+    point_estimates = data[:][args.pdz_col_pe]
+    
+    
+    if two_files:
+        data2 = readTable(args.refz_catalog_file, column_in_refz)
+        reference_values = data2[:][args.refz_col_ref]
+    else:
+        reference_values = data[:][args.refz_col_ref]
+    if args.ref_plot.upper()=="TRUE":
+        logger.info('Computing the reference map')  
+        ref_map = PHZ_PdfHandling.PdfPlot.stackPdfFromBinInRef(pdf_data, reference_values, stack_bins)
+    if args.shift_plot.upper()=="TRUE":
+        logger.info('Computing the shift map')  
+        shift_map,bins = PHZ_PdfHandling.PdfPlot.stackedShiftedPdf(pdf_data, pdz_bins, point_estimates, reference_values, stack_bins)
+    if args.pit_plot.upper()=="TRUE" or args.crps_plot.upper()=="TRUE": 
+        pits, crps  = PHZ_PdfHandling.PdfPlot.computePitAndCrps(pdf_data, pdz_bins, point_estimates, progressCallback)
+      
+      
+    if args.ref_plot.upper()=="TRUE": 
+        f = plt.figure(1)   
+        PHZ_PdfHandling.PdfPlot.plotPdfMap(ref_map, pdz_bins, stack_bins, [0.68, 0.9], norm=LogNorm(vmin=0.01,vmax=np.max(ref_map)))
+        f.show()
+    if args.shift_plot.upper()=="TRUE":
+        f = plt.figure(2)   
+        PHZ_PdfHandling.PdfPlot.plotPdfMap(shift_map, bins, stack_bins, [0.68,0.9] )
+        f.show()
+    if args.pit_plot.upper()=="TRUE":
+        f = plt.figure(3)   
+        PHZ_PdfHandling.PdfPlot.plotPIT(pits, histo_bins=args.hist_bins)
+        f.show()
+    if args.crps_plot.upper()=="TRUE":
+        f = plt.figure(4)   
+        PHZ_PdfHandling.PdfPlot.plotCRPS(crps, histo_bins=args.hist_bins)
+        f.show()
+    input()
+    return Exit.Code["OK"]
