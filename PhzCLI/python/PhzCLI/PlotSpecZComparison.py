adc346983b411b89fd814ce5627171b30fe5d79f
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2020-05-12 11:08:31 +0200

    SpeczPhotozPlot: Custom figsize, density for color
    * The custom figsize is useful for when embedding on a notebook
    * Color based on density makes more obvious where the majority of points
      are

--- PhzCLI/python/PhzCLI/PlotSpecZComparison.py
+++ PhzCLI/python/PhzCLI/PlotSpecZComparison.py
@@ -31,6 +31,8 @@ import matplotlib.animation as animation
 
 import PhzCLI.TableUtils as tut
 
+from scipy.stats import gaussian_kde
+
 # Astropy >= 2.0rc1
 try:
     from astropy.samp import SAMPIntegratedClient
@@ -54,7 +56,7 @@ def read_specz_catalog(filename, id_col, specz_col):
     if not specz_col == 'SPECZ':
         specz_cat.rename_column(specz_col, 'SPECZ')
     return specz_cat[['ID', 'SPECZ']]
-    
+
 #
 #-------------------------------------------------------------------------------
 #
@@ -72,14 +74,14 @@ def read_phosphoros_catalog(out_dir, out_cat, phz_col):
     if phz_col not in phos_cat.colnames:
         raise ValueError('ERROR : Photo-z catalog does not have column with name {}'.format(phz_col))
     phos_cat.rename_column(phz_col, 'PHZ')
-    
+
     cols = ['ID', 'PHZ']
     for col in phos_cat.colnames:
         if col.endswith('-1D-PDF'):
             cols.append(col)
-    
+
     return phos_cat[cols]
-    
+
 #
 #-------------------------------------------------------------------------------
 #
@@ -93,7 +95,7 @@ def get_pdf_bins_from_comment(comment_lines, parameter):
     bins_str = bins_str[bins_str.index('{')+1: bins_str.index('}')]
     bins = [type_map[parameter](value) for value in bins_str.split(',')]
     return bins
-    
+
 #
 #-------------------------------------------------------------------------------
 #
@@ -106,11 +108,11 @@ def read_pdfs(catalog, out_dir):
                     'LIKELIHOOD-SED': 'likelihood_pdf_sed.fits',
                     'LIKELIHOOD-REDDENING-CURVE': 'likelihood_pdf_red_curve.fits',
                     'LIKELIHOOD-EBV': 'likelihood_pdf_ebv.fits', 'LIKELIHOOD-Z': 'likelihood_pdf_z.fits'}
-    
+
     def read(parameter):
         if parameter+'-1D-PDF' in catalog.colnames:
             print('    ' + parameter + ': Using catalog column ' + parameter+'-1D-PDF')
-            
+
             key_comment = 'COMMENT'
             if key_comment not in catalog.meta.keys():
                 key_comment = 'comments'
@@ -123,50 +125,50 @@ def read_pdfs(catalog, out_dir):
         else:
             logger.error('    ' + parameter + ': No 1D PDF found')
             return None
-    
+
     result = [read(p) for p in ['SED', 'REDDENING-CURVE', 'EBV', 'Z',
                                 'LIKELIHOOD-SED', 'LIKELIHOOD-REDDENING-CURVE', 'LIKELIHOOD-EBV', 'LIKELIHOOD-Z']]
     result = [r for r in result if r is not None]
     return result
-    
+
 #
 #-------------------------------------------------------------------------------
 #
-    
+
 def compute_stats(specz, phz):
     """
     Computes mean, median, sigma and outliers 
     
     Returns outliers array
     """
-    
+
     diffArr = phz - specz
-    plusArr = 1 + specz  
+    plusArr = 1 + specz
     dataArr = diffArr / plusArr
-    
+
     mean     = np.average(dataArr)
     median   = np.median(dataArr)
     sigma    = np.std(dataArr)
-    
+
     # Mean absolute deviation
-    mad = np.median(abs(dataArr - median)) 
-    
+    mad = np.median(abs(dataArr - median))
+
     absDataArr = abs(dataArr)
     outliers = [i for i in absDataArr if i > 0.15]
     outliersPercent =  len(outliers)*100. / len(phz)
- 
+
     # Without outliers
     noOutliersArr   = [i for i in absDataArr if i <= 0.15]
     sigmaNoOutliers = np.std(noOutliersArr)
     meanNoOutliers  = np.average(noOutliersArr)
 
-    print('--> Mean                : ', mean)     
-    print('--> Median              : ', median)     
-    print('--> Sigma               : ', sigma)     
-    print('--> Mad                 : ', mad)     
+    print('--> Mean                : ', mean)
+    print('--> Median              : ', median)
+    print('--> Sigma               : ', sigma)
+    print('--> Mad                 : ', mad)
     print('--> Outliers            : ', outliersPercent, '%')
-    print('--> Sigma (no outliers) : ', sigmaNoOutliers)     
-   
+    print('--> Sigma (no outliers) : ', sigmaNoOutliers)
+
     return dataArr, mean, median, sigma, mad, outliersPercent, sigmaNoOutliers, meanNoOutliers
 
 #
@@ -174,10 +176,14 @@ def compute_stats(specz, phz):
 #
 
 class SpeczPhotozPlot(object):
-    
+
     def _createSpeczPhotozPlot(self):
         self.spzPhzAx = plt.subplot2grid((4,3), (0,0), rowspan=3, colspan=3)
         self.spzPhzAx.set_ylabel('PhotoZ')
+        # Plot the data
+        self.spzPhzAx.scatter(self.specz, self.photz, c=self.density, marker='o', s=10, cmap='jet', picker=True)
+        self.spzPhzAx.set_xlim([-0.1, self.z_max])
+        self.spzPhzAx.set_ylim([-0.1, self.z_max])
         # Plot the diagonal
         self.spzPhzAx.plot([-0.1, self.z_max], [-0.1, self.z_max], c='k', alpha=0.2)
         # Plot the outlier limits
@@ -186,29 +192,25 @@ class SpeczPhotozPlot(object):
         y2 = [-0.15+.85*x for x in x_r]
         self.spzPhzAx.plot(x_r, y1, "r", alpha=.3)
         self.spzPhzAx.plot(x_r, y2, "r", alpha=.3)
-        # Plot the data
-        self.spzPhzAx.scatter(self.specz, self.photz, c='b', marker='o', s=10, edgecolors='none', alpha=0.6, picker=True)
-        self.spzPhzAx.set_xlim([-0.1, self.z_max])
-        self.spzPhzAx.set_ylim([-0.1, self.z_max])
-    
+
     def _createDeltaZPlot(self):
         self.deltaZAx = plt.subplot2grid((4,3), (3,0), colspan=3)
         self.deltaZAx.set_ylabel('$\\Delta$z/(1+z)')
         self.deltaZAx.set_xlabel('SpecZ')
+        self.deltaZAx.scatter(self.specz, self.data, c=self.density, marker='o', s=10, cmap='jet', picker=True)
         # Plot the zero line
         self.deltaZAx.plot ([-0.1, self.z_max], [0, 0], c='k', alpha=0.2)
         # Plot the outlier limits
         self.deltaZAx.plot ([-0.1, self.z_max], [0.15, 0.15], c='r', alpha=0.3)
         self.deltaZAx.plot ([-0.1, self.z_max], [-0.15, -0.15], c='r', alpha=0.3)
-        self.deltaZAx.scatter(self.specz, self.data, c='b', marker='o', s=10, edgecolors='none', alpha=0.6, picker=True)
         self.deltaZAx.set_xlim([-0.1, self.z_max])
         self.deltaZAx.set_ylim([-self.data_max, self.data_max])
-        
+
     def _createSelected(self):
         self.id_text = self.spzPhzAx.text(0.05, 0.95, 'selected ID:', transform=self.spzPhzAx.transAxes, va='top')
         self.selected1 = self.spzPhzAx.scatter(0., 0., c='none', marker='o', s=60., edgecolors='#00EE00', linewidth=2., visible=False)
         self.selected2 = self.deltaZAx.scatter(0., 0., c='none', marker='o', s=60., edgecolors='#00EE00', linewidth=2., visible=False)
-    
+
     def _updatePlots(self):
         if self.selected_index is None:
             return []
@@ -218,32 +220,36 @@ class SpeczPhotozPlot(object):
         self.selected2.set_visible(True)
         self.selected2.set_offsets([self.specz[self.selected_index], self.data[self.selected_index]])
         return self.id_text, self.selected1, self.selected2
-    
+
     def _makeTightLayout(self):
         self.fig.tight_layout()
         self.fig.subplots_adjust(hspace=0)
 
-    def __init__(self, ids, specz, photz, data):
+    def __init__(self, ids, specz, photz, data, figsize=None):
         self.ids = ids
         self.specz = specz
         self.photz = photz
         self.data = data
-        
-        self.fig = plt.figure(figsize=(7, 8))
+
+        self.fig = plt.figure(figsize=figsize)
         self.z_max = max(max(specz), max(photz)) + 0.1
         self.data_max = max(max(data), abs(min(data))) + 0.1
-        
+        xy = np.vstack([self.specz, self.photz])
+        self.density = gaussian_kde(xy)(xy)
+        z_order = np.argsort(self.density)
+        self.specz, self.photz, self.density = self.specz[z_order], self.photz[z_order], self.density[z_order]
+
         self._createSpeczPhotozPlot()
         self._createDeltaZPlot()
         self._createSelected()
-        
+
         self._makeTightLayout()
         self.fig.canvas.mpl_connect('resize_event', lambda x:self._makeTightLayout())
         self.canvas = self.fig.canvas
-        
+
         self.selected_index = None
         self.anim = animation.FuncAnimation(self.fig, lambda n:self._updatePlots(), interval=100, blit=True, repeat=True)
-    
+
     def updateSelectedRow(self, index):
         self.selected_index = index
 
@@ -253,7 +259,7 @@ class SpeczPhotozPlot(object):
 
 def displayHistogram(data, mean, median, mad, sigma, outliersPercent, sigmaNoOutliers, meanNoOutliers):
 
-    f, ax = plt.subplots(figsize=(10, 4)) 
+    f, ax = plt.subplots(figsize=(10, 4))
 
     plt.hist(data, bins=100)
     ax.axvline(x=0.15, c='r', alpha=.3)
@@ -261,12 +267,12 @@ def displayHistogram(data, mean, median, mad, sigma, outliersPercent, sigmaNoOut
     ax.set_xlabel("Value")
     ax.set_ylabel("Frequency")
     ax.set_title('Distribution of : (PhotoZ - SpecZ)/(1 + SpecZ)')
-              
+
     # Write information
     txt = '\n  Mean : %2.5f\n  Median : %2.5f\n  Mad : %2.5f\n  Sigma : %2.5f\n  Outliers : %2.5f%%\n  Sigma(no outliers) : %2.5f\n  Mean((no outliers) : %2.5f ' \
         % (mean, median, mad, sigma, outliersPercent, sigmaNoOutliers, meanNoOutliers)
     ax.text(ax.get_xlim()[0], ax.get_ylim()[1], txt, fontsize=10, family='sans-serif', style='italic', ha='left', va='top', alpha=.5)
-    
+
     f.tight_layout()
     f.canvas.mpl_connect('resize_event', lambda x: f.tight_layout())
     return f
@@ -276,14 +282,14 @@ def displayHistogram(data, mean, median, mad, sigma, outliersPercent, sigmaNoOut
 #
 
 class PdfPlot(object):
-    
+
     def _updatePlot(self):
         if self.selected_index is None:
             return []
-        
+
         self.lines.set_ydata(self.pdf_list[self.selected_index])
         self.ax.set_ylim(0, max(self.pdf_list[self.selected_index])*1.05)
-        
+
         if self.parameter == 'Z':
             y_lim = self.ax.get_ylim()
             y_size = y_lim[1]-y_lim[0]
@@ -321,66 +327,66 @@ class PdfPlot(object):
             self.ax.set_xticklabels(labels, rotation='vertical')
         self.fig.tight_layout()
         self.fig.canvas.mpl_connect('resize_event', lambda x:self.fig.tight_layout())
-    
+
     def _initZLines(self):
         self.speczLine, = self.ax.plot([0, 0], [0, 0], c='k', alpha=.5, visible=False)
         self.speczText = self.ax.text(0, 0, 'SpecZ', color='k', alpha=.5, visible=False)
         self.phzLine, = self.ax.plot([0, 0], [0, 0], c='r', alpha=.5, visible=False)
         self.phzText = self.ax.text(0, 0, 'PHZ', color='r', alpha=.5, visible=False)
-    
+
     def __init__(self, parameter, bins, pdf_list, catalog, phz_col):
         self.parameter = parameter
         self.bins = bins
         self.pdf_list = pdf_list
         self.catalog = catalog
         self.phz_col = phz_col
-        
+
         self.is_numerical = isinstance(self.bins[0], numbers.Number)
         self.pdf_title_map = {'SED':'SED', 'REDDENING-CURVE':'Reddening Curve', 'EBV':'E(B-V)', 'Z':'Z'}
         self._initFigure()
         self._initZLines()
-        
+
         self.selected_index = None
         self.anim = animation.FuncAnimation(self.fig, lambda n:self._updatePlot(), interval=100, blit=False, repeat=True)
-       
+
     def updateSelectedRow(self, index):
         self.selected_index = index
-        
+
 
 #
 #-------------------------------------------------------------------------------
 #
 
 class Selector(object):
-    
+
     def __init__(self, obj_list, catalog):
         for obj in obj_list:
             if hasattr(obj, 'canvas'):
                 obj.canvas.mpl_connect('pick_event', self.onpick)
         self.obj_list = obj_list
         self.catalog = catalog
-    
+
     def _print_cols(self, index):
         row = self.catalog[index]
         print('\nFull info for source with ID',row['ID'],':')
         for c in row.colnames:
             print('   ', c, '=', row[c])
-    
+
     def onpick(self, event):
         if event.mouseevent.button != 1:
             return
-        
+
         index = event.ind[0]
-        
+
         for obj in self.obj_list:
             if hasattr(obj, 'updateSelectedRow'):
                 obj.updateSelectedRow(index)
-                
+
         if not event.mouseevent.dblclick:
             return
-        
+
         self._print_cols(index)
-            
+
 
 #
 #-------------------------------------------------------------------------------
@@ -408,7 +414,7 @@ class SampUpdater(object):
             self.global_ids_reverse = {}
             for i in range(len(self.global_ids)):
                 self.global_ids_reverse[self.global_ids[i]] = i
-            
+
             self.id_col = []
             self.orig_ids = []
             tablenames = []
@@ -420,7 +426,7 @@ class SampUpdater(object):
             if os.path.exists(phos_out_dir+'/phz_cat.fits'):
                 tablenames.append(phos_out_dir+'/phz_cat.fits')
                 self.id_col.append('ID')
-            
+
             for ti, name in enumerate(tablenames):
                 data = table.Table.read(name, format='fits')
                 self.orig_ids.append(data[self.id_col[ti]])
@@ -509,7 +515,7 @@ def mainMethod(args):
     for k in ['COMMENT', 'comments']:
         if k in specz_cat.meta:
             del specz_cat.meta[k]
-    
+
 
     # merge the catalogs
     logger.info('Merging the catalogs')
@@ -519,27 +525,27 @@ def mainMethod(args):
         logger.critical('No matching objects found between the SpecZ and the PhotoZ catalogs')
         logger.critical('Was the proper ID column chosen?')
         exit(1)
-    
+
 
     specz = catalog['SPECZ']
     phz = catalog['PHZ']
     data, mean, median, sigma, mad, outliersPercent,sigmaNoOutliers, meanNoOutliers = compute_stats(specz, phz)
-    
+
     if args.no_display:
         exit()
 
-    fig1 = SpeczPhotozPlot(catalog['ID'], specz, phz, data)
+    fig1 = SpeczPhotozPlot(catalog['ID'], specz, phz, data, figsize=(7, 8))
     fig2 = displayHistogram(data, mean, median, mad, sigma, outliersPercent, sigmaNoOutliers, meanNoOutliers)
-    
+
     pdfs = read_pdfs(catalog, args.phosphoros_output_dir)
     pdf_plots = [
         PdfPlot(param, bins, pdf_list, catalog, 'PHZ') for param, bins, pdf_list in pdfs if len(bins) > 1
     ]
-    
+
     samp = None
     if args.samp:
         samp = SampUpdater(args.specz_catalog, args.specz_cat_id, args.phosphoros_output_dir, catalog, [fig1] + pdf_plots)
-    
+
     selector = Selector([fig1, samp] + pdf_plots, catalog)
 
     try:
